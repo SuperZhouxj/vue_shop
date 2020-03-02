d2842811187a7f48f73afff147d24524e101dcea
zhouxingjing <zxj5017@163.com>
2020-03-02 15:34:25 +0800

    commit

---
+++ src/components/Login.vue
@@ -0,0 +1,114 @@
+<template>
+  <div class="login_container">
+    <div class="login_box">
+      <!-- 登陆头像 -->
+      <div class="avatar_box">
+        <img src="../assets/logo.png" />
+      </div>
+      <!-- 登陆表格 通过 :rules 属性传入约定的表格验证规则 -->
+      <el-form
+        ref="loginFormRef"
+        label-width="0px"
+        :model="loginForm"
+        :rules="loginFormRules"
+        class="form_login"
+      >
+        <el-form-item prop="username">
+          <el-input v-model="loginForm.username" prefix-icon="iconfont icon-user"></el-input>
+        </el-form-item>
+        <el-form-item prop="password">
+          <el-input
+            v-model="loginForm.password"
+            type="password"
+            prefix-icon="iconfont icon-3702mima"
+          ></el-input>
+        </el-form-item>
+        <el-form-item class="btn">
+          <el-button type="primary" @click="login">登陆</el-button>
+          <el-button type="info" @click="resetForm">重置</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      loginForm: {
+        username: "111",
+        password: "11"
+      },
+      loginFormRules: {
+        username: [
+          { required: true, message: "请输入登陆名称", trigger: "blur" },
+          { min: 3, max: 10, message: "长度在3到10个字符", trigger: "blur" }
+        ],
+        password: [
+          { required: true, message: "请输入登陆密码", trigger: "blur" },
+          { min: 6, max: 15, message: "长度在6到15个字符", trigger: "blur" }
+        ]
+      }
+    };
+  },
+  methods: {
+    // 重置按钮，重置表单数据
+    resetForm() {
+      // console.log(this);
+      this.$refs.loginFormRef.resetFields();
+    },
+    login() {
+      this.$refs.loginFormRef.validate(valid => {
+        console.log(valid);
+      });
+    }
+  }
+};
+</script>
+
+<style lang="less" scoped>
+.login_container {
+  height: 100%;
+  width: 100%;
+  background-color: #2b4b6b;
+}
+.login_box {
+  height: 300px;
+  width: 450px;
+  border-radius: 3px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background-color: #fff;
+}
+.avatar_box {
+  height: 130px;
+  width: 130px;
+  border-radius: 50%;
+  padding: 10px;
+  border: 1px solid #eee;
+  position: absolute;
+  background-color: #fff;
+  box-shadow: 0 0 10px #ddd;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  img {
+    height: 100%;
+    width: 100%;
+    border-radius: 50%;
+    background-color: #eee;
+  }
+}
+.form_login {
+  bottom: 0;
+  width: 100%;
+  padding: 0 30px;
+  box-sizing: border-box;
+  position: absolute;
+}
+.btn {
+  float: right;
+}
+</style>
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,4 +1,10 @@
 import Vue from 'vue'
 import { Button } from 'element-ui'
+import { form } from 'element-ui'
+import { FormItem } from 'element-ui'
+import { input } from 'element-ui'
 
 Vue.use(Button)
+Vue.use(form)
+Vue.use(FormItem)
+Vue.use(input)
