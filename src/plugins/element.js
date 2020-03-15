e0e2cc0f83d58632338b6bc41ec795f221bb46a1
zhouxingjing <zxj5017@163.com>
2020-03-15 13:07:51 +0800

    完成订单模块

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -37,7 +37,9 @@ import {
     Step,
     Checkbox,
     CheckboxGroup,
-    Upload
+    Upload,
+    Timeline,
+    TimelineItem
 } from 'element-ui'
 import upperCase from 'upper-case'
 
@@ -77,5 +79,7 @@ Vue.use(Step)
 Vue.use(Checkbox)
 Vue.use(CheckboxGroup)
 Vue.use(Upload)
+Vue.use(Timeline)
+Vue.use(TimelineItem)
 Vue.prototype.$message = Message
 Vue.prototype.$confirm = MessageBox.confirm
